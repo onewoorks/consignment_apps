ec008a6526c1e59635a1183fb338eb8dd43c4441
irwan ibrahim <irwanbibrahim@onewoorks.com>
2022-05-21 09:44:16 +0800

    add printer sample

--- resources/views/mob/customer/profile.blade.php
+++ resources/views/mob/customer/profile.blade.php
@@ -159,7 +159,7 @@
                     products: product_data
                 }),
                 success: function (data) {
-                    window.location.href = 'my.bluetoothprint.scheme://http://localhost:8000/print-data/sales/1'
+                    window.location.href = 'my.bluetoothprint.scheme://https://ncig.onewoorks-solutions.com/print-data/sales/1'
                 }
             })
             //do ajax call with payload yang dah dibind
